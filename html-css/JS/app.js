c0c8222433327606910372ebb56851140b166c80
Blackstar84 <carlosalmada84@gmail.com>
2022-11-14 19:48:26 -0300

    se agrega el proyecto de make it real html y css

---
+++ html-css/JS/app.js
@@ -0,0 +1,9 @@
+/* alert("Hola Amigo"); */
+// este es un comentario de una línea
+/*
+    este es un comentario 
+    de varias líneas
+    console.log("esto no aparece al ejecutar el archivo")
+*/
+console.log("Hola Mundo!");
+console.log("Esto está muy bacano"); //este es otro comentario
